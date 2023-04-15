3a7af7868a90b94bc4f612b2919b3b9e4848ea4d
ahmed diab <aquapet33@gmail.com>
2023-04-15 18:09:57 +0200

    Task1

---
+++ Task1.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+const Students = [
+    "ahmed",
+    "akram",
+    "mohamed",
+]
+const Studentlist = (req, res) => {
+    let output = "<ul>"
+    for (let i = 0; i < Students.length; i++) {
+        output += "<li>" + Students[i] + "</li>";
+    }
+    output += "</ul>"
+    res.send(output)
+}
+app.get("/students", Studentlist)
+app.listen(3000)
